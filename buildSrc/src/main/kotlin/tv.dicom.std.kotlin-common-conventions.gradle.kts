9c6de893017b99d449159be1b9bb10201df4dc3d
Tom <tomvercaut@gmail.com>
2022-12-01 11:21:00 +0100

    Added a common logging API dependency.
    The logging API (log4j-api-kotlin) was added as a Gradle implementation dependency. The logging implementation (log4j core) was added as a Gradle runtime dependency for testing only.

--- buildSrc/src/main/kotlin/tv.dicom.std.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/tv.dicom.std.kotlin-common-conventions.gradle.kts
@@ -6,9 +6,22 @@ plugins {
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
+
+    maven {
+        url = uri("https://repository.apache.org/snapshots")
+    }
 }
 
 dependencies {
+
+    implementation("org.apache.logging.log4j:log4j-api-kotlin:1.3.0-SNAPSHOT")
+
+    // log4j-core is a runtime dependency
+    // The version of log4j-core was increased to 2.19.0 because it's binary compatible with previous releases.
+    // More info can be found on:
+    // - https://logging.apache.org/log4j/2.x/index.html
+    // - https://logging.apache.org/log4j/kotlin/artifacts.html
+    testRuntimeOnly("org.apache.logging.log4j:log4j-core:2.19.0")
 
     // Use the kotlin.test library which under the hood uses JUnit to run tests.
     testImplementation(kotlin("test"))
