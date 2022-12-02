e7301a42df6a0545c640f67e42baba8c5801321f
Tom Vercauteren <tomvercaut@gmail.com>
2022-12-02 03:08:19 +0100

    Added a function to detect a CIOD table header and changed the testing strategy.
    As part of the TDD approach the test functions fail for unimplemented functionality. A test function to build a CIOD from an XML parts has been added.

--- core/src/main/kotlin/tv/dicom/std/core/parser/DicomStandardBuilder.kt
+++ core/src/main/kotlin/tv/dicom/std/core/parser/DicomStandardBuilder.kt
@@ -1,9 +1,11 @@
 package tv.dicom.std.core.parser
 
+import org.apache.logging.log4j.kotlin.logger
 import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
+import tv.dicom.std.core.Resource
 import tv.dicom.std.core.model.DicomStandard
 import tv.dicom.std.core.model.ciod.Ciod
 import tv.dicom.std.core.model.imd.Imd
@@ -13,6 +15,7 @@ import javax.xml.xpath.XPathFactory
 
 
 val xPathFactory: XPathFactory = XPathFactory.newInstance()
+private val log = logger("DicomStandardBuilder")
 
 /**
  * Build the DicomStandard model from a set of XML documents.
@@ -28,10 +31,12 @@ fun build(documents: List<Document>): Optional<DicomStandard> {
         val rootId = root.getAttribute("id")
 
         if (rootId == "PS3.3") {
-            buildPart03(document, dicomStandard)
+            if (!buildPart03(document, dicomStandard)) {
+                return Optional.empty()
+            }
         }
     }
-    return Optional.empty()
+    return Optional.of(dicomStandard)
 }
 
 
@@ -39,42 +44,108 @@ fun build(documents: List<Document>): Optional<DicomStandard> {
  * Build the DicomStandard model using the XML document from part 03 of the DICOM standard.
  *
  * @param document XML document of the DICOM standard
- * @return If successful, a [DicomStandard] is returned.
- * Otherwise, an empty [Optional] is returned.
+ * @return If successful, true is returned. If something went wrong false is returned.
  */
-fun buildPart03(document: Document, dicomStandard: DicomStandard) {
+fun buildPart03(document: Document, dicomStandard: DicomStandard): Boolean {
     val root = document.documentElement
     // Get Chapter A
     // Composite Information Object Definitions (Normative)
-    val optChapterA = findNode(root, "//chapter[@id='chapter_A']")
+    val optChapterA = findElement(root, "//chapter[@id='chapter_A']")
     if (optChapterA.isEmpty) {
-
-        return
+        log.error(Resource.errorMessage("DicomPart03ChapterAMissing"))
+        return false
     }
     val chapterA = optChapterA.get()
+    buildCiods(root, chapterA)
 
     // Get Chapter C
     // Information Module Definitions (Normative)
-    val optChapterC = findNode(root, "//chapter[@id='chapter_C']")
+    val optChapterC = findElement(root, "//chapter[@id='chapter_C']")
     if (optChapterC.isEmpty) {
-
-        return
+        log.error(Resource.errorMessage("DicomPart03ChapterCMissing"))
+        return false
     }
     val chapterC = optChapterC.get()
+    buildImds(root, chapterC)
 
+    return true
 }
 
 
 fun buildCiods(root: Element, parent: Element): List<Ciod> {
     val ciods = mutableListOf<Ciod>()
 
+    val optTables = findElements(parent, "//table")
+    if (optTables.isEmpty) {
+        return ciods
+    }
+    val tables = optTables.get()
+    for (table in tables) {
+        val opt = buildCiod(root, table)
+        if (opt.isEmpty) {
+            continue
+        }
+        val ciod = opt.get()
+        ciods.add(ciod)
+    }
+
     return ciods
 }
 
-fun buildCiod(root: Element, parent: Element): Ciod {
+fun buildCiod(root: Element, table: Element): Optional<Ciod> {
     val ciod = Ciod()
+    if (table.nodeName != "table") {
+        return Optional.empty()
+    }
+    if (hasCiodTableHeader(table)) {
 
-    return ciod
+    }
+
+    return Optional.of(ciod)
+}
+
+/**
+ * Check if a table header matches with the one from a CIOD table.
+ *
+ * @param table CIOD XML table
+ * @return True if it matches the following criteria:
+ *   * no thead element
+ *   * no tr element
+ *   * the first tr element doesn't contain the
+ */
+private fun hasCiodTableHeader(table: Element): Boolean {
+    val optThead = findElement(table, "thead")
+    if (optThead.isEmpty) {
+        return false
+    }
+    val thead = optThead.get()
+    val optTrs = findElements(thead, "tr")
+    if (optTrs.isEmpty) {
+        return false
+    }
+    val trs = optTrs.get()
+    if (trs.isEmpty()) {
+        return false
+    }
+    for (i in trs.indices) {
+        val optTds = findElements(trs[i], "th")
+        if (optTds.isEmpty) {
+            continue
+        }
+        val tds = optTds.get()
+        if (tds.size != 4) {
+            continue
+        }
+        val ie = tds[0].toString().lowercase()
+        val module = tds[1].toString().lowercase()
+        val reference = tds[2].toString().lowercase()
+        val usage = tds[3].toString().lowercase()
+        if (ie == "ie" || module == "module" || reference == "reference" || usage == "usage") {
+            return true
+        }
+    }
+
+    return true
 }
 
 fun buildImds(root: Element, parent: Element): List<Imd> {
@@ -83,10 +154,10 @@ fun buildImds(root: Element, parent: Element): List<Imd> {
     return imds
 }
 
-fun buildImd(root: Element, parent: Element): Imd {
+fun buildImd(root: Element, parent: Element): Optional<Imd> {
     val imd = Imd()
 
-    return imd
+    return Optional.of(imd)
 }
 
 private fun isElementType(node: Node?): Boolean {
