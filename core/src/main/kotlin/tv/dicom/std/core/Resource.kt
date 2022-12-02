78b73b2278ef192056c5070f47c79639c2badc2a
Tom Vercauteren <tomvercaut@gmail.com>
2022-12-02 02:31:04 +0100

    Added a ResourceBundle and helper class to manage error messages.

---
+++ core/src/main/kotlin/tv/dicom/std/core/Resource.kt
@@ -0,0 +1,56 @@
+package tv.dicom.std.core
+
+import org.apache.logging.log4j.kotlin.logger
+import java.util.Locale
+import java.util.ResourceBundle
+
+/**
+ * Helper class to manage ResourceBundles used in the project.
+ */
+internal class Resource {
+    companion object {
+        @JvmStatic
+        private val log = logger(this::class.java.name)
+
+        @JvmStatic
+        private var _errorResourceBundle: ResourceBundle? = null
+
+        /**
+         * Get the ResourceBundle with error messages.
+         */
+        @JvmStatic
+        val error: ResourceBundle
+            get() {
+                if (_errorResourceBundle == null) {
+                    val local = Locale.getDefault() ?: Locale.ROOT
+                    if (local != null) {
+                        _errorResourceBundle = ResourceBundle.getBundle("errors", local)
+                    } else {
+                        log.error("Unable to get default Locale.")
+                        _errorResourceBundle = ResourceBundle.getBundle("errors")
+                    }
+                }
+                if (_errorResourceBundle == null) {
+                    log.error("Error ResourceBundle is null")
+                    throw NullPointerException("Internal resource bundle is null.")
+                }
+                return _errorResourceBundle!!
+            }
+
+        /**
+         * Get a message from an error ResourceBundle.
+         *
+         * @param key resource property key
+         * @return The error message from the ResourceBundle. An empty error message is returned if a NullPointerException is caught internally.
+         */
+        @JvmStatic
+        fun errorMessage(key: String): String {
+            return try {
+                val rb = this.error
+                rb.getString(key)
+            } catch (ex: NullPointerException) {
+                ""
+            }
+        }
+    }
+}
---
+++ core/src/test/kotlin/tv/dicom/std/core/ResourceTest.kt
@@ -0,0 +1,22 @@
+package tv.dicom.std.core
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class ResourceTest {
+
+    @Test
+    fun getError() {
+        val expected = "ResourceTest"
+        val value = Resource.error.getString("ResourceTest")
+        assertEquals(expected, value)
+    }
+
+    @Test
+    fun getErrorMessage() {
+        val expected = "ResourceTest"
+        val value = Resource.errorMessage("ResourceTest")
+        assertEquals(expected, value)
+    }
+}
