9ee5568a36a7701bca876cce307748b1cb59343e
Tom <tomvercaut@gmail.com>
2022-12-04 15:41:58 +0100

    Added a type designation to an Information Module Definition DataEntry.

--- core/src/main/kotlin/tv/dicom/std/core/model/imd/DataEntry.kt
+++ core/src/main/kotlin/tv/dicom/std/core/model/imd/DataEntry.kt
@@ -4,8 +4,13 @@ import tv.dicom.std.core.model.Tag
 
 data class DataEntry(
     val seqIndent: UShort = 0u,
+    // Attribute name
     var name: String = "",
+    // Data element Tag
     var tag: Tag = Tag(),
+    // Type designation
+    var type: String = "",
+    // Attribute definition
     var description: String = ""
 ) : Entry {
     override fun isSequence(): Boolean {
